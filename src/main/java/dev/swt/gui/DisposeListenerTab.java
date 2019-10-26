87869ed11c97b67b0f904f1e7d4cb22ea2e0735f
Marius <marius@example.com>
2019-10-26 23:47:37 +0200

    [GUI-01] #CREATE 'event:DisposeListenerTab'
    {created listener for disposing tabs.
    not implemented yet.}

---
+++ src/main/java/dev/swt/gui/DisposeListenerTab.java
@@ -0,0 +1,17 @@
+package dev.swt.gui;
+
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+
+public class DisposeListenerTab implements DisposeListener {
+
+    public DisposeListenerTab() {
+
+    }
+
+    @Override
+    public void widgetDisposed(DisposeEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+}
