045f0a49c827a4bd16d6cf3a2585cb46c40347d8
Marius <marius.schenzle@googlemail.com>
2019-11-05 12:23:24 +0100

    [GUI-01] #EXTEND 'app:main'
    {extend main method with supported locales.
    check if console arguments are allowed.}

--- src/main/java/dev/swt/gui/App.java
+++ src/main/java/dev/swt/gui/App.java
@@ -1,5 +1,6 @@
 package dev.swt.gui;
 
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -16,13 +17,20 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        if (args.length == 2) {
-            new Editor(ResourceBundle.getBundle("dev.swt.gui.i18n.MessageBundle", new Locale(args[0], args[1]))).run();
-        } else if (args.length == 1) {
-            new Editor(ResourceBundle.getBundle("dev.swt.gui.i18n.MessageBundle", new Locale(args[0]))).run();
-        } else {
-            new Editor(ResourceBundle.getBundle("dev.swt.gui.i18n.MessageBundle")).run();
-        }
+        Locale[] supportedLocales = { new Locale("en", "US"), new Locale("de", "DE") };
 
+        if (args.length == 2) {
+            Locale currentLocale = new Locale(args[0], args[1]);
+            if (Arrays.asList(supportedLocales).contains(currentLocale)) {
+                new Editor(ResourceBundle.getBundle("dev.swt.gui.MessageBundle", currentLocale)).run();
+            } else {
+                System.out.println("Supported locales:");
+                for (Locale locale : supportedLocales) {
+                    System.out.println(locale);
+                }
+            }
+        } else {
+            System.out.println("Missing Arguements: language region");
+        }
     }
 }
